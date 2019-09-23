64c614432e526f35d6ba91013b68d042de4b288c
Akash Gohel <akash.gohel.37231@gmail.com>
2019-09-23 12:46:26 +0530

    Fundamental of Type Script with React

---
+++ src/useDropdown.tsx
@@ -0,0 +1,28 @@
+import React, { useState, FunctionComponent, Dispatch } from "react";
+
+const useDropdown = (label: string, defaultState: string, options: string[]) => {
+  const [state, updateState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown: FunctionComponent = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={e => updateState(e.target.value)}
+        onBlur={e => updateState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option />
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [state, Dropdown, updateState] as [string, FunctionComponent, Dispatch<string>];
+};
+
+export default useDropdown;
